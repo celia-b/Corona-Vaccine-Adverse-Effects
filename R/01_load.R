fbce8f7c6938ab5ce9bed9370ef4859c69b1d98b
celia-b <celiburgos@gmail.com>
2021-04-23 08:26:20 +0000

    Loaded data - parsing errors fixed

--- R/01_load.R
+++ R/01_load.R
@@ -11,13 +11,44 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_raw <- read_tsv(file = "data/_raw/my_raw_data.tsv")
 
+# When you load 2021VAERSDATA.csv without specifying column types, 
+# some problems arise. I found this is because R infers the column
+# type from the first 1000 rows, which is bad in cases like the column
+# BIRTH_DEFECT, where there is a "Y" in case of "yes" and an NA in case 
+# of "no". Most patients don't have birth defects and so the 1000 first 
+# rows are all NAs and R assigns the column type to be logical (1/0/T/F/TRUE/FALSE)
+# and gets confused when it sees a "Y" and just puts an NA. This means 
+# we lose data so we have to fix it by specifying column types in the loading.
+# The correct column types can be found in the VAERS user guide:
+# https://vaers.hhs.gov/docs/VAERSDataUseGuide_November2020.pdf
+
+patients_raw <- read_csv(file = "data/_raw/2021VAERSDATA.csv", 
+                         col_types = cols("BIRTH_DEFECT" = col_character(),
+                                          "X_STAY" = col_character(),
+                                          "RPT_DATE" = col_date(format="%m/%d/%Y"),
+                                          "V_FUNDBY" = col_character(),
+                                          "ER_VISIT" = col_character()))
+
+symptoms_raw <- read_csv(file = "data/_raw/2021VAERSSYMPTOMS.csv")
+vaccines_raw <- read_csv(file = "data/_raw/2021VAERSVAX.csv")
 
 # Wrangle data ------------------------------------------------------------
-my_data <- my_data_raw # %>% ...
+
+# Looking better into the data, there are not that many columns with no data.
+# Most N/As mean "no" or something similar. This we will have to deal with.
+
+# Removed columns:
+# CAGE_YR and CAGE_MO (calculated patient age in years and month) 
+#   -> empty - we have AGE_YRS instead
+# There are some columns that seem useless like RPT_DATE (date of 
+# completion of the form), but they might be useful for solving
+# row duplications, so let's not remove them yet.
+
+patients <- patients_raw %>%
+  select(-c("CAGE_YR", "CAGE_MO"))
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = my_data,
-          file = "data/01_my_data.tsv")
+write_csv(x = patients,
+          file = "data/01_patients.csv")
