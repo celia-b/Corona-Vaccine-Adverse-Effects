33b77bb03f3fa091f9babca998237be0d9cf3d3b
celia-b <celiburgos@gmail.com>
2021-05-05 09:00:04 +0000

    Visualizations: days after vaccination when symptoms/death occurs

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -4,6 +4,8 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("patchwork")
+library("scales")
 
 
 # Define functions --------------------------------------------------------
@@ -25,50 +27,182 @@ merged_data <- read_csv(file = "data/03_merged_data.csv",
 
 # Visualise data ----------------------------------------------------------
 ######################## SYMPTOMS AFTER N DAYS ############################
-## Distribution of the number of days after vaccine injection
-## when symptoms appear.
 
-merged_data %>%
+## Distribution of the number of days after receiving the vaccine
+## when symptoms appear.
+symptoms_after <- merged_data %>%
   select(VAERS_ID, AGE_CLASS, SEX, SYMPTOMS_AFTER, VAX_MANU) %>%
   arrange(SYMPTOMS_AFTER) %>%
   filter(SYMPTOMS_AFTER < 20) %>%
-  ggplot(aes(SYMPTOMS_AFTER)) +
-  geom_bar() # would be nice to have x-axis as proportion instead of count
+  ggplot(aes(SYMPTOMS_AFTER, stat(prop))) +
+  geom_bar() +
+  scale_y_continuous(labels = percent) +
+  labs(x = "Days after vaccination",
+       y = "Relative ocurrence")
 
-# It's interesting that there is a bump at around 7 days, which is when 
-# the adaptive immune system kick in. Maybe symptoms that appear immediately
-# after the vaccine is recieved are due to the innate immune response
-# and the ones after a few days are the adaptive.
 
-# By age class 
-# --> there is a problem here with unequal representation of age classes
-merged_data %>%
+
+## By age class --> I think we need less age classes
+# Also I'm not convinced this plot looks nice --> maybe facet_wrap it?
+symptoms_after_age <- merged_data %>%
   select(VAERS_ID, AGE_CLASS, SEX, SYMPTOMS_AFTER, VAX_MANU) %>%
   arrange(SYMPTOMS_AFTER) %>%
   filter(SYMPTOMS_AFTER < 20) %>%
   drop_na(AGE_CLASS) %>%
-  ggplot(aes(SYMPTOMS_AFTER, fill = AGE_CLASS)) +
-  geom_bar()
+  ggplot(aes(SYMPTOMS_AFTER, stat(prop), fill = AGE_CLASS)) +
+  geom_bar(position = "dodge") +
+  #scale_y_continuous(labels = percent) +
+  labs(#subtitle = "Grouping by age class",
+       x = "Days after vaccination",
+       y = "Relative ocurrence") +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank()) +
+  labs(fill = "Age class")
 
-# By manufacturer 
-# --> again, problem with manufacturer representation
-merged_data %>%
+
+
+# Putting them together
+(symptoms_after + symptoms_after_age) + 
+  plot_annotation(title = 'Days after vaccination when symptoms appear',
+                  subtitle = NULL,
+                  caption = "(Symptoms that appear after day 20 are infrequent and not shown)") +
+  theme(plot.title = element_text(size=24))
+
+
+
+## By manufacturer 
+symptoms_after_manu <- merged_data %>%
   select(VAERS_ID, AGE_CLASS, SEX, SYMPTOMS_AFTER, VAX_MANU) %>%
   arrange(SYMPTOMS_AFTER) %>%
   filter(SYMPTOMS_AFTER < 20) %>%
-  ggplot(aes(SYMPTOMS_AFTER, fill = VAX_MANU)) +
-  geom_bar()
+  ggplot(aes(SYMPTOMS_AFTER, stat(prop), fill = VAX_MANU)) +
+  geom_bar(position = "dodge") +
+  scale_y_continuous(labels = percent) +
+  labs(title = "Days after vaccination when symptoms appear.",
+       subtitle = "Grouping by manufacturer",
+       caption = NULL,
+       tag = NULL,
+       x = "Days after vaccination",
+       y = "Relative ocurrence") +
+  labs(fill = "Manufacturer")
+
+symptoms_after_manu
 
 
-######################## NUMBER OF SYMPTOMS ###############################
+
+######################## DEATH AFTER N DAYS ############################
+
+## Days after vaccination when symptoms appear.
+death_after <- merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED_AFTER, VAX_MANU) %>%
+  arrange(DIED_AFTER) %>%
+  filter(DIED_AFTER < 30 & DIED_AFTER >= 0) %>%
+  ggplot(aes(DIED_AFTER, stat(prop))) +
+  geom_bar() +
+  scale_y_continuous(labels = percent) +
+  labs(title = "Days after vaccination when death occurs",
+       caption = "(Death after 30 days is infrequent and not shown)",
+       x = "Days after vaccination",
+       y = "Relative ocurrence")
+
+death_after
+
+
+## By age class --> Not informative (small sample size, < 5 in age classes)
 merged_data %>%
-  select(VAERS_ID, AGE_CLASS, SEX, N_SYMPTOMS, VAX_MANU) %>%
-  arrange(N_SYMPTOMS) %>%
-  filter(N_SYMPTOMS < 20) %>%
-  ggplot(aes(N_SYMPTOMS)) + 
-  geom_bar()
+  select(VAERS_ID, AGE_CLASS, SEX, DIED_AFTER, VAX_MANU) %>%
+  arrange(DIED_AFTER) %>%
+  filter(DIED_AFTER < 30 & DIED_AFTER >= 0) %>%
+  drop_na(AGE_CLASS) %>%
+  group_by(AGE_CLASS) %>%
+  count()
 
-  
+
+## By manufacturer --> Again problematic because of small sample size in Janssen
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED_AFTER, VAX_MANU) %>%
+  arrange(DIED_AFTER) %>%
+  filter(DIED_AFTER < 30 & DIED_AFTER >= 0) %>%
+  group_by(VAX_MANU) %>%
+  count() # --> 16 (Janssen) vs 852 vs 690 (Moderna and Pfizer)
+
+n_days_manu <- merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED_AFTER, VAX_MANU) %>%
+  arrange(DIED_AFTER) %>%
+  filter(DIED_AFTER < 30 & DIED_AFTER >= 0) %>%
+  ggplot(aes(DIED_AFTER, stat(prop), fill = VAX_MANU)) +
+  geom_bar(position = "dodge") +
+  scale_y_continuous(labels = percent) +
+  labs(title = "Days after vaccination when death occurs.",
+       subtitle = "Grouping by manufacturer",
+       caption = NULL,
+       tag = NULL,
+       x = "Days after vaccination",
+       y = "Relative ocurrence") +
+  labs(fill = "Manufacturer")
+
+n_days_manu
+
+
+
+######################## DEATH RATE ###############################
+# Out of the people who died, what proportion were already sick?
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED, HAS_ILLNESS, VAX_MANU) %>%
+  filter(DIED == 'Y') %>%
+  ggplot(aes(HAS_ILLNESS)) + 
+  geom_bar() +
+  coord_flip()
+
+# How much more likely is it to die when you were already sick when 
+# taking the vaccine vs when you were healthy? --> DO STATISTICAL ANALYSIS
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED, HAS_ILLNESS, VAX_MANU) %>%
+  group_by(HAS_ILLNESS) %>%
+  count()
+
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED, HAS_ILLNESS, VAX_MANU) %>%
+  group_by(HAS_ILLNESS, DIED) %>%
+  count()
+
+prop_test(x = c(1191, 644), n = c(29127, 4306), 
+          p = NULL, alternative = "two.sided", correct = TRUE)
+
+
+# Out of the people who died, what proportion had covid at the time?
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED, HAS_COVID, VAX_MANU) %>%
+  filter(DIED == 'Y') %>%
+  ggplot(aes(HAS_COVID)) + 
+  geom_bar() +
+  coord_flip()
+
+
+# How much more likely is it to die when you had covid while 
+# taking the vaccine vs when you were healthy? --> DO STATISTICAL ANALYSIS
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED, HAS_COVID, VAX_MANU) %>%
+  group_by(HAS_COVID) %>%
+  count()
+
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED, HAS_COVID, VAX_MANU) %>%
+  group_by(HAS_COVID, DIED) %>%
+  count()
+
+# How much less likely is it to die when you had covid in the past? 
+# --> DO STATISTICAL ANALYSIS
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED, HAD_COVID, VAX_MANU) %>%
+  group_by(HAD_COVID) %>%
+  count()
+
+merged_data %>%
+  select(VAERS_ID, AGE_CLASS, SEX, DIED, HAD_COVID, VAX_MANU) %>%
+  group_by(HAD_COVID, DIED) %>%
+  count()
 
 
 # Write data --------------------------------------------------------------
