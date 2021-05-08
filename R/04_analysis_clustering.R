0978894bd0aea98f686f580710a1b88301cd6054
isadpc <s202406@student.dtu.dk>
2021-05-08 08:19:28 +0000

    PCA and kmeans update

--- R/04_analysis_clustering.R
+++ R/04_analysis_clustering.R
@@ -10,6 +10,7 @@ library("scales")
 library("broom")
 library("purrr")
 library("infer")
+library("tidymodels")
 
 
 # Define functions --------------------------------------------------------
@@ -22,101 +23,80 @@ source(file = "R/99_project_functions.R")
 merged_data_wide <- read_csv(file = gzfile("data/03_merged_data_wide.csv.gz"),
                              col_types = cols(VAX_DOSE_SERIES = col_character()))
 
-# Long format symptom column
-merged_data_long <- read_csv(file = gzfile("data/03_merged_data_long.csv.gz"), 
-                             col_types = cols(HOSPDAYS = col_integer(),
-                                              DIED_AFTER = col_integer(), 
-                                              VAX_DOSE_SERIES = col_character()))
 
 # Wrangle data ------------------------------------------------------------
 
-# Convert variables to factors
-merged_data_wide <- merged_data_wide %>% 
-  mutate_if(is.character, as.factor) %>%
-  mutate_if(is.logical, as.factor)
 
-# Convert variables to factors
-merged_data_long <- merged_data_long %>%
-  mutate_if(is.character, as.factor) %>%
-  mutate_if(is.logical, as.factor)
-
-# Define symptoms
-symptoms <- merged_data_wide %>% 
-  select(DYSPNOEA, PAIN_IN_EXTREMITY, DIZZINESS, FATIGUE, 
-         INJECTION_SITE_ERYTHEMA, INJECTION_SITE_PRURITUS, INJECTION_SITE_SWELLING, 
-         CHILLS, RASH, HEADACHE, INJECTION_SITE_PAIN, NAUSEA,PAIN, PYREXIA, MYALGIA,
-         ARTHRALGIA, PRURITUS, ASTHENIA, VOMITING, DEATH) %>%
-  names()
-
-
-
-##################### Principal component analysis #############################
-
-# Change all variables I need to numeric and do PCA
-pca_fit <- merged_data_wide %>% 
+# Convert symptom related variables to numeric
+numeric_symptoms <- merged_data_wide %>% 
   mutate (DIED = case_when (DIED == 'N'~ 0,
                             DIED == 'Y'~ 1)) %>%
-  mutate (L_THREAT = case_when (L_THREAT == 'N'~ 0,
-                                L_THREAT == 'Y'~ 1)) %>%
   mutate (HOSPITAL = case_when (HOSPITAL == 'N'~ 0,
                                 HOSPITAL == 'Y'~ 1)) %>%
   mutate (DISABLE = case_when (DISABLE == 'N'~ 0,
-                                DISABLE == 'Y'~ 1)) %>%
+                               DISABLE == 'Y'~ 1)) %>%
   mutate (ER_ED_VISIT = case_when (ER_ED_VISIT == 'N'~ 0,
-                                ER_ED_VISIT == 'Y'~ 1)) %>%
+                                   ER_ED_VISIT == 'Y'~ 1)) %>%
   mutate (DYSPNOEA = case_when (DYSPNOEA == FALSE ~ 0,
                                 DYSPNOEA == TRUE ~ 1)) %>%
   mutate (PAIN_IN_EXTREMITY = case_when (PAIN_IN_EXTREMITY == FALSE ~ 0,
-                                PAIN_IN_EXTREMITY == TRUE ~ 1)) %>%
+                                         PAIN_IN_EXTREMITY == TRUE ~ 1)) %>%
   mutate (DIZZINESS = case_when (DIZZINESS == FALSE ~ 0,
-                                DIZZINESS == TRUE ~ 1)) %>%
+                                 DIZZINESS == TRUE ~ 1)) %>%
   mutate (FATIGUE = case_when (FATIGUE == FALSE ~ 0,
-                                FATIGUE == TRUE ~ 1)) %>%
+                               FATIGUE == TRUE ~ 1)) %>%
   mutate (INJECTION_SITE_ERYTHEMA = case_when (INJECTION_SITE_ERYTHEMA == FALSE ~ 0,
-                                INJECTION_SITE_ERYTHEMA == TRUE ~ 1)) %>%
+                                               INJECTION_SITE_ERYTHEMA == TRUE ~ 1)) %>%
   mutate (INJECTION_SITE_PRURITUS = case_when (INJECTION_SITE_PRURITUS == FALSE ~ 0,
                                                INJECTION_SITE_PRURITUS == TRUE ~ 1)) %>%
   mutate (INJECTION_SITE_SWELLING = case_when (INJECTION_SITE_SWELLING == FALSE ~ 0,
                                                INJECTION_SITE_SWELLING == TRUE ~ 1)) %>%
   mutate (CHILLS = case_when (CHILLS == FALSE ~ 0,
-                               CHILLS == TRUE ~ 1)) %>%
+                              CHILLS == TRUE ~ 1)) %>%
   mutate (RASH = case_when (RASH == FALSE ~ 0,
-                               RASH == TRUE ~ 1)) %>%
+                            RASH == TRUE ~ 1)) %>%
   mutate (HEADACHE = case_when (HEADACHE == FALSE ~ 0,
-                               HEADACHE == TRUE ~ 1)) %>%
+                                HEADACHE == TRUE ~ 1)) %>%
   mutate (INJECTION_SITE_PAIN = case_when (INJECTION_SITE_PAIN == FALSE ~ 0,
-                               INJECTION_SITE_PAIN == TRUE ~ 1)) %>%
+                                           INJECTION_SITE_PAIN == TRUE ~ 1)) %>%
   mutate (NAUSEA = case_when (NAUSEA == FALSE ~ 0,
-                               NAUSEA == TRUE ~ 1)) %>%
+                              NAUSEA == TRUE ~ 1)) %>%
   mutate (PAIN = case_when (PAIN == FALSE ~ 0,
-                               PAIN == TRUE ~ 1)) %>%
+                            PAIN == TRUE ~ 1)) %>%
   mutate (PYREXIA = case_when (PYREXIA == FALSE ~ 0,
                                PYREXIA == TRUE ~ 1)) %>%
   mutate (MYALGIA = case_when (MYALGIA == FALSE ~ 0,
                                MYALGIA == TRUE ~ 1)) %>%
   mutate (ARTHRALGIA = case_when (ARTHRALGIA == FALSE ~ 0,
-                               ARTHRALGIA == TRUE ~ 1)) %>%
+                                  ARTHRALGIA == TRUE ~ 1)) %>%
   mutate (PRURITUS = case_when (PRURITUS == FALSE ~ 0,
-                               PRURITUS == TRUE ~ 1)) %>%
+                                PRURITUS == TRUE ~ 1)) %>%
   mutate (ASTHENIA = case_when (ASTHENIA == FALSE ~ 0,
-                               ASTHENIA == TRUE ~ 1)) %>%
+                                ASTHENIA == TRUE ~ 1)) %>%
   mutate (VOMITING = case_when (VOMITING == FALSE ~ 0,
-                               VOMITING == TRUE ~ 1)) %>% # didn't add death bc it's already in DIED
-  select (DIED, L_THREAT, HOSPITAL, DISABLE, ER_ED_VISIT, SYMPTOMS_AFTER,
+                                VOMITING == TRUE ~ 1)) %>% # didn't add death bc it's already in DIED
+  select (DIED, HOSPITAL, DISABLE, ER_ED_VISIT, SYMPTOMS_AFTER,
           N_SYMPTOMS, DYSPNOEA, PAIN_IN_EXTREMITY, DIZZINESS, FATIGUE, 
           INJECTION_SITE_ERYTHEMA, INJECTION_SITE_PRURITUS, INJECTION_SITE_SWELLING,
           CHILLS, RASH, HEADACHE, INJECTION_SITE_PAIN, NAUSEA, PAIN, PYREXIA, 
           MYALGIA, ARTHRALGIA, PRURITUS, ASTHENIA, VOMITING) %>%
-  drop_na(SYMPTOMS_AFTER) %>% # there are NAs in this column
-  select(where(is.numeric)) %>% # retain only numeric columns
+  drop_na()
+
+# Get classes (vaccine manufacturer)
+classes <- merged_data_wide %>%
+  drop_na (DIED, HOSPITAL, DISABLE, ER_ED_VISIT, SYMPTOMS_AFTER,
+          N_SYMPTOMS, DYSPNOEA, PAIN_IN_EXTREMITY, DIZZINESS, FATIGUE, 
+          INJECTION_SITE_ERYTHEMA, INJECTION_SITE_PRURITUS, 
+          INJECTION_SITE_SWELLING, CHILLS, RASH, HEADACHE, INJECTION_SITE_PAIN, 
+          NAUSEA, PAIN, PYREXIA, MYALGIA, ARTHRALGIA, PRURITUS, ASTHENIA, 
+          VOMITING) %>%
+  select (VAX_MANU)
+
+# Principal component analysis -------------------------------------------------
+
+pca_fit <- numeric_symptoms %>% 
   scale() %>% # scale data
   prcomp(center = TRUE) # do PCA
-
-# instead of doing this the NAs in SYMPTOMS_AFTER should be removed in 
-# 02_clean??
-classes <- merged_data_wide %>% 
-  drop_na(SYMPTOMS_AFTER) %>%
-  select (VAX_MANU)  
 
 # PC1 vs PC2 biplot
 pca_fit %>%
@@ -125,13 +105,12 @@ pca_fit %>%
   geom_point(size = 0.5) +
   labs(x = 'PC1', y = 'PC2') +
   scale_color_viridis_d(name = "MANUFACTURER", option = "D") +
-  theme_half_open(font_size = 9, font_family = "serif, Times") +
+  theme_half_open(font_size = 9, font_family = "Avenir") +
   background_grid() 
   
 # define arrow style for plotting
-arrow_style <- arrow(
-  angle = 10, ends = "first", type = "open", length = grid::unit(5, "pt")
-)
+arrow_style <- arrow(angle = 10, ends = "first", type = "open", 
+                     length = grid::unit(5, "pt"))
 
 # plot rotation matrix
 pca_fit %>%
@@ -144,7 +123,7 @@ pca_fit %>%
     hjust = 0, nudge_x = 0.05, 
     color = "#904C2F",
     size = 2.5) +
-  xlim(-.75, .75) + ylim(-.75, .75) +
+  xlim(-.5, .5) + ylim(-.5, .5) +
   coord_fixed() + # fix aspect ratio to 1:1
   theme_minimal_grid(10)
 
@@ -154,15 +133,35 @@ pca_fit %>%
   ggplot(aes(PC, percent)) +
   geom_col(fill = "#56B4E9", alpha = 0.8) +
   labs(y = "explained variance") +
-  scale_x_continuous(breaks = 1:26) +
+  scale_x_continuous(breaks = 1:25) +
   scale_y_continuous(
     labels = scales::percent_format(),
     expand = expansion(mult = c(0, 0.01))
   ) +
   theme_minimal_hgrid(10)
 
+# K-means clustering  -----------------------------------------------------
 
+# need to keep working on it 
+# including binary variables
+kclust <- kmeans (numeric_symptoms, centers = 3)
 
+class_cluster <- augment(kclust, classes)
+
+class_cluster %>% ggplot(aes(.cluster, fill = VAX_MANU)) +
+  geom_bar(position = "dodge") 
+
+# only number of symptoms and symptoms_after
+
+kclust <- numeric_symptoms %>%
+  select (SYMPTOMS_AFTER, N_SYMPTOMS) %>% 
+  kmeans (centers = 3)
+
+class_cluster <- augment(kclust, classes)
+
+class_cluster %>% ggplot(aes(.cluster, fill = VAX_MANU)) +
+  geom_bar() 
+  
 # Write data --------------------------------------------------------------
 write_tsv(...)
 ggsave(...)
