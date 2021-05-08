8073315ce0535ad263360ae6aa21c0e1533e5a6c
isadpc <s202406@student.dtu.dk>
2021-05-08 10:32:37 +0000

    Added plots to presentation

--- R/04_analysis_clustering.R
+++ R/04_analysis_clustering.R
@@ -115,7 +115,7 @@ arrow_style <- arrow(angle = 10, ends = "first", type = "open",
                      length = grid::unit(5, "pt"))
 
 # plot rotation matrix
-pca_fit %>%
+rotation_matrix <- pca_fit %>%
   tidy(matrix = "rotation") %>%
   pivot_wider(names_from = "PC", names_prefix = "PC", values_from = "value") %>%
   ggplot(aes(PC1, PC2)) +
@@ -129,8 +129,10 @@ pca_fit %>%
   coord_fixed() + # fix aspect ratio to 1:1
   theme_minimal_grid(10)
 
+rotation_matrix
+
 # scree plot
-pca_fit %>%
+scree_plot <- pca_fit %>%
   tidy(matrix = "eigenvalues") %>%
   ggplot(aes(PC, percent)) +
   geom_col(fill = "#56B4E9", alpha = 0.8) +
@@ -141,6 +143,8 @@ pca_fit %>%
     expand = expansion(mult = c(0, 0.01))
   ) +
   theme_minimal_hgrid(10)
+
+scree_plot
 
 # K-means clustering  -----------------------------------------------------
 
@@ -168,5 +172,8 @@ class_cluster %>% ggplot(aes(.cluster, fill = VAX_MANU)) +
 write_tsv(...)
 ggsave(...)
 
+ggsave (biplot, file = "results/biplot.png")
+ggsave (rotation_matrix, file = "results/rotation_matrix.png")
+ggsave (scree_plot, file = "results/scree_plot.png")
 
 
