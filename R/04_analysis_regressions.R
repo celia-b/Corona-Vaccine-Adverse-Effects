8b20e9037f3e4873cfcc3fcb4f32a6ffe435c909
celia-b <celiburgos@gmail.com>
2021-05-09 14:06:31 +0000

    Many logistic regressions (symptoms vs takes antiinflamatory) - model + visualizations

--- R/04_analysis_regressions.R
+++ R/04_analysis_regressions.R
@@ -11,6 +11,7 @@ library("broom")
 library("purrr")
 library("infer")
 library("viridis")
+library("ggrepel")
 
 
 # Define functions --------------------------------------------------------
@@ -221,9 +222,85 @@ death_v_symptoms_interactions <- merged_data_wide %>%
            INJECTION_SITE_SWELLING + CHILLS + RASH + HEADACHE + INJECTION_SITE_PAIN +
            NAUSEA + PAIN + PYREXIA + MYALGIA + ARTHRALGIA + PRURITUS + ASTHENIA + 
            VOMITING)^2, 
-      family = binomial)
+      family = binomial) %>%
+  tidy() %>%
+  mutate(odds_ratio = exp(estimate))
 
-summary(death_v_symptoms_interactions)
+
+
+
+## Many LogRegs ---------------------------------------------
+# How much more/less likely is it to get each of the symptoms
+# if you have taken an antiinflamatory?
+symptoms_v_antiinflamatory_model <- merged_data_long %>%
+  select(TAKES_ANTIINFLAMATORY, SYMPTOM, SYMPTOM_VALUE) %>%
+  group_by(SYMPTOM) %>%
+  nest %>% 
+  ungroup %>%
+  mutate(mdl = map(data, ~glm(TAKES_ANTIINFLAMATORY ~ SYMPTOM_VALUE,
+                              data = .x,
+                              family = binomial(link = "logit")))) %>%
+  mutate(mdl_tidy = map(mdl, ~tidy(.x, conf.int = TRUE))) %>% 
+  unnest(mdl_tidy) %>%
+  filter(term != "(Intercept)") %>%
+  mutate(odds_ratio = exp(estimate)) %>%
+  mutate(identified_as = case_when(p.value < 0.05 ~ "Significant",
+                                   TRUE ~ "Non-significant"),
+         symptom_label = case_when(identified_as == "Significant" ~ as.character(SYMPTOM),
+                                   identified_as == "Non-significant" ~ "")) %>% 
+  mutate(neg_log10_p = -log10(p.value)) %>%
+  select(-c(data, mdl)) # Nested columns are removed bc otherwise we can't save the csv
+
+### Many LogRegs Visualization 1 ---------------------------
+symptoms_v_antiinflamatory_model_fig_manhattan <- symptoms_v_antiinflamatory_model %>% 
+  ggplot(aes(x = SYMPTOM,
+             y = neg_log10_p,
+             colour = identified_as,
+             label = symptom_label)) + 
+  geom_point(alpha = 0.5,
+             size = 2) +
+  geom_hline(yintercept = -log10(0.05),
+             linetype = "dashed") +
+  geom_text_repel(size = 5) +
+  scale_fill_viridis_d() +
+  theme_minimal(base_family = "Avenir") +
+  theme(axis.text.x = element_blank(),
+        legend.position = "bottom",
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5),
+        plot.margin = margin(10, 10, 10, 20)) +
+  labs(title = "P-values for symptom ~ takes anti-inflamatory association",
+       subtitle = "Dashed line indicates a p-value of 0.05",
+       x = "Symptom",
+       y = "-log10(p)")
+
+### Many LogRegs Visualization 2 ---------------------------
+symptoms_v_antiinflamatory_model_fig_odds <- symptoms_v_antiinflamatory_model %>%
+  filter(p.value < 0.05) %>%
+  filter(term != "(Intercept)") %>%
+  ggplot(aes(x = fct_reorder(SYMPTOM, estimate),
+             y = estimate,
+             fill = term)) +
+  geom_bar(stat = "identity") +
+  geom_hline(yintercept = 0,
+             linetype = "dashed", 
+             color = "black") +
+  scale_fill_viridis_d() +
+  labs(title = "Log-Odds ratio for symptom ~ takes anti-inflamatory",
+       subtitle = "A Log-Odds ratio above 0 means the symtom is more common people who take anti-inflamatories",
+       x = "Symptoms",
+       y = "Log-Odds ratio",
+       caption = "Only symptoms with a p-value < 0.05 are shown.") +
+  theme_minimal(base_family = "Avenir") +
+  theme(legend.position = "none", 
+        axis.text.x = element_text(angle = 45, 
+                                   hjust = 1, 
+                                   size = 9), 
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5),
+        plot.margin = margin(10, 10, 10, 20))
+  
+
 
 
 # Write data --------------------------------------------------------------
@@ -251,6 +328,17 @@ ggsave(death_v_symptoms_model_fig_odds,
        height = 5,
        width = 10)
 
+# Symptoms vs. anti-inflamatory
+write_csv(symptoms_v_antiinflamatory_model,
+          file = "results/symptoms_v_antiinflamatory_model.csv")
+ggsave(symptoms_v_antiinflamatory_model_fig_manhattan, 
+       file = "results/symptoms_v_antiinflamatory_model_fig_manhattan.png",
+       height = 5,
+       width = 10)
+ggsave(symptoms_v_antiinflamatory_model_fig_odds, 
+       file = "results/symptoms_v_antiinflamatory_model_fig_odds.png",
+       height = 5,
+       width = 10)
 
 
 
